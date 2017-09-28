14688561e71284bb6b2716b35d71b78587c53d14
fredlam7 <fredlam7@gmail.com>
2017-09-27 20:36:46 -0500

    chat example complete, package from ddesktop an folders corrected

--- public/js/main.js
+++ public/js/main.js
@@ -4,11 +4,19 @@ var path = require('path');
 var io = require('socket.io')(http);
 
 app.get('/', function (req,res){
-  res.sendFile(path.join(__dirname,'../public','index.html'));
+  res.sendFile(path.join(__dirname,'../','index.html'));
 });
 
 io.on('connection',function(socket){
-  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
